8f9a53b1a0e1b52746dfa805272118a381f4dffb
nathanhleung <nathanhleung@users.noreply.github.com>
2017-06-17 13:20:23 -0400

    add rudimentary proof of work

--- src/Lib.hs
+++ src/Lib.hs
@@ -11,6 +11,7 @@ import           Crypto.Hash                    ( Digest
                                                 , digestFromByteString
                                                 )
 import           Crypto.Hash.SHA256
+import           Text.Read                      (readMaybe)
 import           Data.Aeson
 import           Data.Binary
 import           Data.ByteString.Char8          (pack)
@@ -23,6 +24,7 @@ data Block = Block { index        :: Int
                    , previousHash :: String
                    , timestamp    :: Int
                    , blockData    :: String
+                   , nonce        :: Int
                    , blockHash    :: String
                    } deriving (Show, Eq, Generic)
 
@@ -46,22 +48,51 @@ sha256 = digestFromByteString . hash . pack
 -- abstracted hash function that takes a string
 -- to hash and returns a hex string
 hashString :: String -> String
-hashString = maybe (error "Something went wrong generating a hash") show . sha256
+hashString =
+  maybe (error "Something went wrong generating a hash") show . sha256
 
 calculateBlockHash :: Block -> String
-calculateBlockHash (Block i p t b _)  = hashString $ concat [show i, p, show t, b]
+calculateBlockHash (Block i p t b n _)  =
+  hashString $ concat [show i, p, show t, b, show n]
 
 -- returns a copy of the block with the hash set
 addHashToBlock :: Block -> Block
 addHashToBlock block = block { blockHash = calculateBlockHash block }
+
+-- Rudimentary proof-of-work: ensures that a block hash
+-- is less than a certain value (i.e. contains a certain
+-- amount of leading zeroes)
+-- In our case, it's 4 leading zeroes. We're using the Integer type
+-- since the current target is higher than the max for Int
+difficultyTarget :: Integer
+difficultyTarget =
+  0x0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+-- checks whether the provided block hash satisfies
+-- our PoW requirement
+satisfiesPow :: String -> Bool
+satisfiesPow blockHash = maybe
+  (error $ "Something is wrong with the provided hash: " ++ blockHash)
+  (< difficultyTarget)
+  (readMaybe ("0x" ++ blockHash) :: Maybe Integer)
+
+-- Recursively finds a nonce that satisfies the difficulty target
+-- If our blockHash already satisfies the PoW, return the current nonce
+-- If not, increment the nonce and try again
+findNonce :: Block -> Int
+findNonce block = do
+  let blockHash     = calculateBlockHash block
+      currentNonce  = nonce block
+  if satisfiesPow blockHash
+    then currentNonce
+    else findNonce $ block { nonce = currentNonce + 1 }
 
 -- a hardcoded initial block, we need this to make sure all
 -- nodes have the same starting point, so we have a hard coded
 -- frame of reference to detect validity
 initialBlock :: Block
 initialBlock = do
-  let block = Block 0 "0" 0 "initial data" ""
-  block { blockHash = calculateBlockHash block }
+  let block = Block 0 "0" 0 "initial data" 0 ""
+  addHashToBlock $ block { nonce = findNonce block }
 
 -- a new block is valid if its index is 1 higher, its
 -- previous hash points to our last block, and its hash is computed
@@ -70,7 +101,8 @@ isValidNewBlock :: Block -> Block -> Bool
 isValidNewBlock prev next
   | index prev + 1 == index next &&
     blockHash prev == previousHash next &&
-    blockHash next == calculateBlockHash next = True
+    blockHash next == calculateBlockHash next &&
+    satisfiesPow (blockHash next) = True
   | otherwise = False
 
 -- a chain is valid if it starts with our hardcoded initial
@@ -88,9 +120,11 @@ isValidChain chain = case chain of
 mineBlockFrom :: (MonadIO m) => Block -> String -> m Block
 mineBlockFrom lastBlock stringData = do
   time <- liftIO epoch
-  return . addHashToBlock $ Block { index        = index lastBlock + 1
-                                  , previousHash = blockHash lastBlock
-                                  , timestamp    = time
-                                  , blockData    = stringData
-                                  , blockHash    = "will be changed"
-                                  }
+  let block = Block { index        = index lastBlock + 1
+                    , previousHash = blockHash lastBlock
+                    , timestamp    = time
+                    , blockData    = stringData
+                    , nonce        = 0
+                    , blockHash    = "will be changed"
+                    }
+  return . addHashToBlock $ block { nonce = findNonce block }
